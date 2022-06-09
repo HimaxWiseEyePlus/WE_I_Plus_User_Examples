fba1e36cf18d20f364e8abd7aa4c54185e11d4dd
Kris <kris_yeh@himax.com.tw>
2022-06-09 10:13:13 +0800

    Add Edge Impulse Object detection example

---
+++ Edge_Impulse_Object_detection_Example/README.md
@@ -0,0 +1,82 @@
+# Edge Impulse object detection with centroids (FOMO) project build from C++ library example
+Edge Impulse object detection with centroids project (FOMO) can be deployed as a C++ library. This example shows how to export the model from your Edge Impulse project, build it with the HIMAX WE1 SDK and deploy it to HIMAX WE1 EVB. 
+
+## Prerequisites
+- Development Toolkit
+  - This example only supports GNU Development Toolkit. Please check the GNU Development Toolkit chapter [here](https://github.com/HimaxWiseEyePlus/himax_tflm#prerequisites) to prepare the environment to build the example.
+- Download related third party data (only need to download once)
+    ```bash
+    make download
+    ```
+
+## Edge Impulse object detection project setup.
+Please check [here](https://docs.edgeimpulse.com/docs) and [here](https://docs.edgeimpulse.com/docs/himax-we-i-plus#all-licenses-are-in-use-by-other-developers) for Himax WE-I Plus to build your own Edge Impulse project. The Edge impulse detect objects with centroids FOMO document can be reference [here](https://docs.edgeimpulse.com/docs/tutorials/detect-objects-using-fomo). In this example, we use a Mouse Detection project to demonstrate. The following shows the outline of the Impulse design in this project.
+- Input Image data with your own label
+  - Image width = 160
+  - Image height = 160
+  - Image channel = 1
+    ![](images/train_input.png)
+- Output features
+  - 1 (mouse)
+- At `Create impulse` page, you can set your own input and output setting.
+  ![](images/create_impulse.png)
+## Export C++ library to repository
+- Head over to your Edge Impulse project, go to `Deployment` page, select `C++ library `. 
+
+  ![](images/deployment.png)
+
+- Be careful about the Ram usage. It should not be bigger than the HIMAX WE1 SRAM constraint.
+
+  ![](images/deployment_EON_ram_usage.png)
+
+- Then, select `Quantized (int8)` and click `Build` to download the `.zip` file of your project.
+
+  ![](images/deployment_EON.png)
+
+- Extract the `.zip` file and copy the `model-parameters` , `edge-impulse-sdk` and `tflite-model` folders to this example folder, then your folder structure should look like this:
+    ```
+    WE_I_Plus_User_Examples/Edge_Impulse_Object_detection_Example/
+    |_edge-impulse-sdk
+    |_himax_we1_sdk
+    |_image_gen_linux
+    |_images
+    |_main.cc
+    |_Makefile
+    |_memory.x
+    |_memory.lcf
+    |_model-parameters
+    |_README.md
+    |_tflite-model
+    |_third_party
+    ```
+- Your folder structure of `WE_I_Plus_User_Examples/Edge_Impulse_Object_detection_Example/` should be looks like 
+
+  ![](images/folder_structure.png)
+
+
+## Build example and flash image
+- Build Edge Impulse example and flash image. Flash image name will be `edgeimpulse_example.img`. 
+    ```
+    make all
+    make flash
+    ```
+- Flash Image Update at Linux Environment you can reference [here](https://github.com/HimaxWiseEyePlus/bsp_tflu/tree/master/HIMAX_WE1_EVB_user_guide#flash-image-update-at-linux-environment).
+- After above steps, update `edgeimpulse_example.img` to HIMAX WE1 EVB.
+  - In our example, we display the detect object postition on the console(minicom).
+
+    ![](images/console_result.png)
+  - You can see your detect results with a 50*50 pixel box on the PC TOOL. Because this Edge Impulse FOMO model can detect object with only centroids [here](https://docs.edgeimpulse.com/docs/tutorials/detect-objects-using-fomo) for more detail.
+
+    - Before you click `PC_TOOL`, you can download [here](https://github.com/HimaxWiseEyePlus/WE_I_Plus_User_Examples/releases/download/v1.0/PC_TOOL), please key-in following command on console.
+      ```  
+      chmod 777 PC_TOOL
+      ```
+    - Install FT4222 linux driver
+
+      - You can reference the user guide [here](https://github.com/HimaxWiseEyePlus/bsp_tflu/tree/master/HIMAX_WE1_EVB_user_guide#update-bootloader-version-at-linux-environment) to download and install FT4222 linux driver.
+    - Click `PC_TOOL` under your folder.
+      ![](images/pc_tool.png)
+    - Please click `Recv` button
+      ![](images/linux_pc_tool.png)
+    - Then you will see your detect results with a 50*50 pixel box.
+      ![](images/pc_tool_result.png)
---
+++ Edge_Impulse_Object_detection_Example/main.cc
@@ -0,0 +1,214 @@
+#include "hx_drv_tflm.h"
+#include "tflite-model/trained_model_compiled.h"
+#include "edge-impulse-sdk/classifier/ei_run_classifier.h"
+#include "model-parameters/model_variables.h"
+#define MAX_TRACKED_ALGO_RES  10
+typedef enum
+{
+	MONO_FRAME=0,
+	RAWBAYER_FRAME,
+	YUV_FRAME
+}enum_frameFormat;
+
+
+typedef struct
+{
+    uint32_t x;
+    uint32_t y;
+    uint32_t width;
+    uint32_t height;
+}struct__box;
+
+typedef struct
+{
+	struct__box bbox;
+    uint32_t time_of_existence;
+    uint32_t is_reliable;
+}struct_MotionTarget;
+
+
+
+typedef struct
+{
+	struct__box upper_body_bbox;
+    uint32_t upper_body_scale;
+    uint32_t upper_body_score;
+    uint32_t upper_body_num_frames_since_last_redetection_attempt;
+    struct__box head_bbox;
+    uint32_t head_scale;
+    uint32_t head_score;
+    uint32_t head_num_frames_since_last_redetection_attempt;
+    uint32_t octave;
+    uint32_t time_of_existence;
+    uint32_t isVerified;
+}struct_Human;
+
+typedef struct
+{
+    int num_hot_pixels ;
+    struct_MotionTarget Emt[MAX_TRACKED_ALGO_RES]; ; //ecv::motion::Target* *tracked_moving_targets;
+    int frame_count ;
+    short num_tracked_moving_targets;
+    short num_tracked_human_targets ;
+    bool humanPresence ;
+    struct_Human ht[MAX_TRACKED_ALGO_RES];  //TrackedHumanTarget* *tracked_human_targets;
+    int num_reliable_moving_targets;
+    int verifiedHumansExist;
+}struct_algoResult;
+
+typedef struct boxabs {
+    float left, right, top, bot;
+} boxabs;
+struct_algoResult algoresult;
+namespace
+{
+    hx_drv_sensor_image_config_t g_pimg_config;
+    hx_drv_gpio_config_t gpio_config;
+    int input_width = 160;
+    int input_height = 160;
+    int input_channels = 1;
+}
+
+// Callback function declaration
+static int get_signal_data(size_t offset, size_t length, float *out_ptr);
+int8_t inference_image[25600]={0};//input_channels*input_width*input_height
+TfLiteStatus GetImage(int image_width, int image_height, int channels, int8_t *image_data)
+{
+    static bool is_initialized = false;
+
+    if (!is_initialized)
+    {
+        if (hx_drv_sensor_initial(&g_pimg_config) != HX_DRV_LIB_PASS)
+        {
+            return kTfLiteError;
+        }
+
+        if (hx_drv_spim_init() != HX_DRV_LIB_PASS)
+        {
+            return kTfLiteError;
+        }
+
+        is_initialized = true;
+    }
+
+    //capture image by sensor
+    hx_drv_sensor_capture(&g_pimg_config);
+
+    //send jpeg image data out through SPI
+    hx_drv_spim_send(g_pimg_config.jpeg_address, g_pimg_config.jpeg_size,
+                     SPI_TYPE_JPG);
+
+    //rescale image to model input size
+    hx_drv_image_rescale((uint8_t *)g_pimg_config.raw_address,
+                         g_pimg_config.img_width, g_pimg_config.img_height,
+                         image_data, image_width, image_height);
+
+    return kTfLiteOk;
+}
+extern "C" void print_out(const char *format, va_list args){
+    hx_drv_uart_print("%s", format);
+};
+//link to label define array
+extern const char* ei_classifier_inferencing_categories [];
+int main(void)
+{
+    // initial uart
+    hx_drv_uart_initial(UART_BR_115200);
+    // loop step
+    while (true)
+    {
+         if (kTfLiteOk != GetImage(input_width, input_height, input_channels,inference_image))
+        {
+            hx_drv_uart_print("Image capture failed.");
+            continue;
+        }
+        signal_t signal;            // Wrapper for raw input buffer
+        ei_impulse_result_t result; // Used to store inference output
+        EI_IMPULSE_ERROR res;       // Return code from inference
+
+        // Assign callback function to fill buffer used for preprocessing/inference
+        signal.total_length = EI_CLASSIFIER_DSP_INPUT_FRAME_SIZE;
+        signal.get_data = &get_signal_data;
+
+        // Perform DSP pre-processing and inference
+        res = run_classifier(&signal, &result, false);
+        int bbox_quantity = 0;
+        // Print the prediction results (object detection)
+        hx_drv_uart_print("Object detection: \r\n");
+        for (uint32_t i = 0; i < EI_CLASSIFIER_OBJECT_DETECTION_COUNT; i++) {
+            ei_impulse_result_bounding_box_t bb = result.bounding_boxes[i];
+            if (bb.value == 0) {
+                continue;
+            }
+            int score = bb.value*100;
+        #if 1
+            algoresult.ht[i].upper_body_score = (uint32_t)(score);
+            algoresult.ht[i].upper_body_bbox.x = (uint32_t)(bb.x  * g_pimg_config.img_width / input_width);
+            algoresult.ht[i].upper_body_bbox.y = (uint32_t)(bb.y * g_pimg_config.img_height / input_height);
+            // set 50 x 50 box to show on PC_TOOL
+            algoresult.ht[i].upper_body_bbox.width = 50;
+            algoresult.ht[i].upper_body_bbox.height = 50;
+            hx_drv_uart_print(" %s score: (%d) [ x: %u, y: %u]\r\n", 
+                    bb.label,
+                    score, 
+                    algoresult.ht[i].upper_body_bbox.x, 
+                    algoresult.ht[i].upper_body_bbox.y);
+                
+            bbox_quantity++;
+        #endif
+        
+        }
+
+        if (bbox_quantity>0)
+        {
+            algoresult.humanPresence = true;
+            algoresult.num_tracked_human_targets = bbox_quantity;
+        }
+        else
+        {
+            hx_drv_uart_print(" no object\r\n");
+            algoresult.humanPresence = false;
+            algoresult.num_tracked_human_targets = 0;
+        }
+        if(hx_drv_spim_send((uint32_t)&algoresult, sizeof(struct_algoResult), SPI_TYPE_META_DATA ) != HX_DRV_LIB_PASS)
+        {
+            hx_drv_uart_print("spi send bbox failed.");
+            return 0;
+        }
+
+    }
+
+    return 0;
+}
+
+void grayscale_to_rgb(uint8_t gray, uint8_t *r, uint8_t *g, uint8_t *b) {
+    *r = gray;
+    *g = gray;
+    *b = gray;
+}
+
+int get_signal_data(size_t offset, size_t length, float *out_ptr) {
+    size_t bytes_left = length;
+    size_t out_ptr_ix = 0;
+
+    // read byte for byte
+    while (bytes_left != 0) {
+
+        // grab the value and convert to r/g/b
+        uint8_t pixel = inference_image[offset]+128;
+
+        uint8_t r, g, b;
+        grayscale_to_rgb(pixel, &r, &g, &b);
+        // then convert to out_ptr format
+        float pixel_f = (r << 16) + (g << 8) + b;
+        out_ptr[out_ptr_ix] = pixel_f;
+
+        // and go to the next pixel
+        out_ptr_ix++;
+        offset++;
+        bytes_left--;
+    }
+
+    // and done!
+    return 0;
+}
